0033a61ff97c420598ee7c5ad390b8cbc10f655f
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2018-04-21 14:03:03 +0200

    Don't rely on inline JavaScript
    The policy for the deployed site disallow inline JavaScript.

--- webroot/toggle-description.js
+++ webroot/toggle-description.js
@@ -1,3 +1,15 @@
+// Wait until the DOM is ready before registering event listeners
+document.addEventListener('DOMContentLoaded', onLoaded, false);
+
+function onLoaded() {
+    var checkbox = document.getElementById('description-checkbox');
+    // Register so we update when checkbox is checked or unchecked
+    checkbox.addEventListener('change', toggleDescription, false);
+    // Sometimes the browser remembers the state of the checkbox. Ensure we are
+    // in sync with it upon loading.
+    toggleDescription({target: checkbox});
+}
+
 function toggleDescription(event) {
     var checkbox = event.target;
     var content = document.getElementById('content');
